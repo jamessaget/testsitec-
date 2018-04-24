38b52df9fb97577b49eefea0c83567fcd3d3a7d0
James <jamsee16@aol.com>
2018-04-24 18:35:40 +0100

    started to create functionality

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random ()
+        {
+
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id) {
+
+            return Content("id: " + id);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month) {
+
+            return Content(year + "/" + month);
+
+        }
+
+    }
+}
