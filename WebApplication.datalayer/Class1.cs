287e44288377bb4859231bac71d053cb3323c15c
James Saget <jamsee16@aol.com>
2018-05-27 19:48:33 +0100

    added DBcontext in separate project

---
+++ WebApplication.datalayer/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WebApplication1.Models;
+
+namespace WebApplication.datalayer
+{
+    public class Class1 : DbContext
+    {
+        public DbSet<Product> Products {get; set;}
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
